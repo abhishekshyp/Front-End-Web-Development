7c430c71e10a789734f44fb4e0f3e3c33ca2a823
Abhishek Kashyap <71214929+abhishekshyp@users.noreply.github.com>
2022-06-04 00:57:25 +0530

    Add files via upload

---
+++ Dashboard Project/assets/js/script.js
@@ -0,0 +1,17 @@
+const sideMenu = document.querySelector("aside"),
+                menuBtn = document.querySelector("#menu-btn"),
+                closeBtn = document.querySelector("#close-btn"),
+                themeToggler = document.querySelector(".theme-toggler");
+
+            menuBtn.addEventListener('click', () => {
+                sideMenu.style.display = 'block';
+            });
+            closeBtn.addEventListener('click', () => {
+                sideMenu.style.display = 'none';
+            });
+            themeToggler.addEventListener('click', () => {
+                document.body.classList.toggle('dark-theme-variables');
+
+                themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+                themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+            });
